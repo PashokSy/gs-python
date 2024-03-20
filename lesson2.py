2779b89e965ab95276d14fd0779fd3e6517f0fb3
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-03-20 18:11:48 +0200

    feat: Core of Python
    - lesson 1
    - lesson 2

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+name = "Tom"
+_name = 'Tom'
+
+num_1 = 6
+num_2 = 3
+
+num3 = num_1 % num_2
+print(num3)
+
+print(num_1 == num_2)
+print(num_1 > num_2)
+
+num = 10
+name = 'Tom'
+
+result = num > 5 and name == 'Tom'
+print(result)
+
+result = num > 5 or name == 'Tom'
+print(result)
+
